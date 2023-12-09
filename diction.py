6913d6db8aa000827514e5889443d9c4be84fc2c
Mathias Oluwatoyin <111644894+damilareoluwat5@users.noreply.github.com>
2023-12-10 00:56:42 +0100

    Add files via upload

---
+++ diction.py
@@ -0,0 +1,10 @@
+
+europe={"spain":"madrid","france":'paris','germany':'berlin','norway':'oslo'}
+europe['nigeria']='lagos'
+europe['ghana']='accra'
+europe['nigeria']='abuja'
+#delete ghana'
+del europe['ghana']
+#ghana deleted╩
+print(europe)
+
